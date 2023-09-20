d71ea439b74e3a4863f779503f2426da6dfa91b9
zhangrc <zhangrc2023@163.com>
2023-09-20 20:14:41 +0800

    feat:app.vue

---
+++ my-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+        <h1>个人页面</h1>
+        <img alt="图片" src="./assets/profile.png">
+        <h2>姓名：张荣聪</h2>
+        <h2>昵称：Zhang bro</h2>
+        <h2>学号：2310273074</h2>
+        <h2>专业：计算机技术</h2>
+        <h2>爱好：电影、经济学书籍</h2>
+    </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
